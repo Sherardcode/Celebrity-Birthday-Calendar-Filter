65a07d6c99d7c7e4c20a65e492dfcefef0549fcd
Showman645 <128947295+Sherardcode@users.noreply.github.com>
2026-08-24 16:00:47 -0500

    Create Request.cs

---
+++ Request.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheduler
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public string Category { get; set; } = "";
+        public DateTime Birthday { get; set; }
+
+     
+    }
+}
